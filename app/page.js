35190863cc75bdb899e5c41954d74b1bbb65f036
chetan sharma <cs7804@gmail.com>
2024-05-20 10:17:01 +0530

    changed attributes like fullscreen to fullScreen for the iframe

--- app/page.js
+++ app/page.js
@@ -53,7 +53,7 @@ export default function Home() {
       
       <div className=" text-white py-10 flex flex-col gap-8 items-center">
         <h1 className="text-center text-2xl font-bold">Learn more about Us</h1>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/fjHO4fAfCf0?si=7LOXPiktDd4oHGaC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/fjHO4fAfCf0?si=7LOXPiktDd4oHGaC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
       </div>
     </>
   );
